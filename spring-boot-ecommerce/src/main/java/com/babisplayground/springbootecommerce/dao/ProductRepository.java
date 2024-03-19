2fc5b26f10a3ad7fc00c544ca645d5c12ab1bcce
AbenezerM <abenezermulugeta41@gmail.com>
2024-03-19 01:29:06 -0400

    Add CORS policy to the dao objects to allow requests coming from an angular client.

--- spring-boot-ecommerce/src/main/java/com/babisplayground/springbootecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/babisplayground/springbootecommerce/dao/ProductRepository.java
@@ -2,9 +2,10 @@ package com.babisplayground.springbootecommerce.dao;
 
 import com.babisplayground.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
